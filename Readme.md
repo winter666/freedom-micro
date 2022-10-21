0fbafcbc4f0c04699fe012855be8d785b671110b
Davut <sergeevdavut@mail.ru>
2022-10-21 18:01:29 +0300

    Added Env module && update readme

--- Readme.md
+++ Readme.md
@@ -1,6 +1,6 @@
 # Freedom framework
 
-## Configurations for your project
+## Easy to configure your custom project settings
 
 - path ```/config/<config_name>.php```
 - get ```Config::getInstance()->get('<config_name>')```
@@ -9,3 +9,5 @@
   "param 1" => "value1",
   "param 2" => "value2",
   ]```
+
+## Using .env file
---
+++ src/Modules/Dotenv/Env.php
@@ -0,0 +1,47 @@
+<?php
+
+
+namespace Winter666\Freedom\Modules\Dotenv;
+
+use Winter666\Freedom\Modules\Config\Config;
+
+class Env
+{
+    protected string $name;
+
+    public function __construct(string $name = '.env')
+    {
+        $this->name = $name;
+    }
+
+    public static function getInstance(): static {
+        return new static;
+    }
+
+    public function getAll(): array {
+        $config = Config::getInstance()->get('server');
+        $fileContent = file_get_contents(
+            str_replace($config['public_path'],
+                '',
+                $_SERVER['DOCUMENT_ROOT']). '/' . $this->name
+        );
+
+        $rows = explode("\n", $fileContent);
+        $data = [];
+        foreach ($rows as $row) {
+            $item = explode('=', $row);
+            $name = $item[0];
+            $value = $item[1] ?? null;
+            if (!empty($name) && empty($data[$name])) {
+                $data[trim($name)] = trim($value);
+            }
+        }
+
+        return $data;
+    }
+
+    public function get(string $name): string|null {
+        $all = $this->getAll();
+        return $all[$name] ?? null;
+    }
+}
